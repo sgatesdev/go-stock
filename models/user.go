b072f0852b41cef35586060784f1cd7981405d40
sgatesdev <sam.j.gates@gmail.com>
2024-01-16 21:38:34 -0500

    Reverted change to User model.

--- models/user.go
+++ models/user.go
@@ -1,9 +1,9 @@
 package models
 
 type User struct {
-	ID       string  `json:"id" gorm:"primary_key"`
-	Email    string  `json:"email" gorm:"not null"`
-	Password string  `json:"password" gorm:"not null"`
-	Stocks   []Stock `json:"stocks" gorm:"foreignKey:ID"`
-	Username string  `json:"username" gorm:"not null"`
+	ID       string `json:"id" gorm:"primary_key"`
+	Email    string `json:"email" gorm:"not null"`
+	Password string `json:"password" gorm:"not null"`
+	// Stocks   []Stock `json:"stocks" gorm:"foreignKey:ID"`
+	Username string `json:"username" gorm:"not null"`
 }
